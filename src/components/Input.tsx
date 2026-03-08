1ea8a6e4d3d8782e96803896ddd4c220acf60317
Hypirius <zuhayerhossain25@gmail.com>
2026-03-08 22:48:23 +0600

    refactor: add support for different input types

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,26 @@
-import { type SetStateAction, type Dispatch, type ChangeEvent } from "react"
+import {
+  type SetStateAction,
+  type Dispatch,
+  type ChangeEvent,
+  type HTMLInputTypeAttribute,
+} from "react"
 
 type InputProps = {
   updateStateFn: Dispatch<SetStateAction<string>>
   value: string
   id?: string
+  type?: HTMLInputTypeAttribute
 }
 
-export default function Input({ updateStateFn, value, id }: InputProps) {
+export default function Input({ updateStateFn, value, id, type }: InputProps) {
   function handleOnChange(e: ChangeEvent<HTMLInputElement>) {
     updateStateFn(e.target.value)
   }
+
   return (
     <input
       onChange={handleOnChange}
-      type="text"
+      type={type ? type : "text"}
       required
       value={value}
       id={id ? id : ""}
